dc971d0beb06fd49e459ba49635db9dcbd8cea56
석준 문 <moona0915@gmail.com>
2019-10-07 21:14:33 +0900

    Update swap2.c
    indent 수정

--- 기초컴퓨터프로그래밍/5주차/swap2.c
+++ 기초컴퓨터프로그래밍/5주차/swap2.c
@@ -2,20 +2,20 @@
 
 #include <stdio.h> 
 
-int main(void)
-{
+int main(void) {
     int a[10] = {11, 22, 33, 44, 55};
     int i, j, n = 5;
     
     scanf("%d", &j);
     
     if (j < 1 || j > n) {
-    	j = n;
-	}
+        j = n;
+    }
+
     for(i = j - 1; i < n; i++) {
-    	a[i] = a [i + 1];
-	}
-	n--;
+        a[i] = a [i + 1];
+    }
+    n--;
     
     for(i = 0; i < n; i++) printf("%d ", a[i]);
     printf("\n");
